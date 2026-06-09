bb7ff68da9fafb35d16b2876aae4deda2cdaa3f6
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-09 10:57:29 +0530

    add database connection and migration functionality

---
+++ internal/database/db.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"mini-wallet/internal/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+
+	if err != nil {
+		log.Fatal("failed to connect to DB")
+	}
+
+	DB = db
+
+	fmt.Println("Database connected")
+	err = DB.AutoMigrate(
+		&models.User{},
+	)
+
+	if err != nil {
+		log.Fatal("migration failed")
+	}
+
+	fmt.Println("migration successful!")
+}
