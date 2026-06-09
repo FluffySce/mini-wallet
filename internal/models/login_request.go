2c548995ee7c899a7a33374cf9b36547d0ca975a
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-09 16:22:57 +0530

    fix: update comments in LoginRequest for clarity on email sensitivity and wallet ID issue

--- internal/models/login_request.go
+++ internal/models/login_request.go
@@ -1,5 +1,8 @@
 package models
 
+// email is case sensitive it shouldnt be
+// everyone is getting wallet id as 0, i.e singular wallet must be a relation ship issue
+
 type LoginRequest struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
