03ea6bb2895dd35fc30821874f9c32ef88542422
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-09 11:56:55 +0530

    add SetupRoute function to define application routes

---
+++ internal/routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"mini-wallet/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoute(router *gin.Engine) {
+	router.GET(
+		"/health",
+		handlers.HealthCheck,
+	)
+	router.POST(
+		"/register",
+		handlers.Register,
+	)
+}
