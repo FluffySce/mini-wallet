b56dff27a1536549e1c967dfe27cce05c4d2bf63
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-09 11:56:26 +0530

    add route handling to main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"mini-wallet/config"
 	"mini-wallet/internal/database"
+	"mini-wallet/internal/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv()
 	database.ConnectDB()
+
+	// route handling
+	router := gin.Default()
+	routes.SetupRoute(router)
+	router.Run(":8000")
 }
