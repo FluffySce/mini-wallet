5c05dd6dbfd918c55d63bef7e1ca296f32206828
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-10 17:08:12 +0530

    feat: implement login functionality with email and password input

--- mini-wallet-frontend/src/app/login/page.tsx
+++ mini-wallet-frontend/src/app/login/page.tsx
@@ -5,9 +5,47 @@ import {
   CardHeader,
   CardTitle,
   CardDescription,
+  CardContent,
 } from "@/components/ui/card";
 
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { api } from "@/lib/api";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  // log-in handler logic
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      setError("");
+
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", response.data.token);
+      router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+      setError("Invalid email or password.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Card className="w-full max-w-md p-6">
@@ -15,6 +53,35 @@ export default function LoginPage() {
           <CardTitle>Mini Wallet</CardTitle>
           <CardDescription>Login to continue</CardDescription>
         </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="alice@mail.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></Input>
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></Input>
+            </div>
+            {error && <p className="text-sm text-red-500">{error}</p>}
+            <Button className="w-full" onClick={handleLogin} disabled={loading}>
+              {loading ? "Logging in..." : "Login"}
+            </Button>
+          </div>
+        </CardContent>
       </Card>
     </div>
   );
