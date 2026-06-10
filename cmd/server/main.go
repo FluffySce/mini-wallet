03b975f0adb4bdf6afcfdab31c991a5ed2e14d21
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-06-10 17:07:59 +0530

    feat: add CORS support for API with configuration for local development

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,9 @@ import (
 	"mini-wallet/config"
 	"mini-wallet/internal/database"
 	"mini-wallet/internal/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +16,21 @@ func main() {
 
 	// route handling
 	router := gin.Default()
+
+	//cors init
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin", "Content-Type", "Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	routes.SetupRoute(router)
 	router.Run(":8000")
 }
